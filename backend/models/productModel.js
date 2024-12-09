20570e2de403696c5ad99bf805ef6701fff222b4
ephraim tetteh <aetech.hub@aetechorg.com>
2024-12-09 14:56:29 +0000

    updating temiperi files to git

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,7 +4,11 @@ const productSchema = new mongoose.Schema({
    name: {type: String, require: true},
    price: {
       type: Number, 
-      require: [true, 'product price must be provided']
+      require: [true, 'product price must be provided'],
+      enum: {
+         retails_price: 0,
+         whole_sale_price: 0
+      }
    },
    quantity: {type: Number, require: true},
    category: {type: String, require: true},
