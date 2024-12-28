4160107796b0ec08ce8bb71ea5180ac0a131a151
ephraim <aetech.hub@aetechorg.com>
2024-12-28 00:42:41 +0000

    adding updated files in backend

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,18 +1,14 @@
 import mongoose, { mongo } from "mongoose";
 
 const productSchema = new mongoose.Schema({
-   name: {type: String, require: true},
-   price: {
-      type: Number, 
-      require: [true, 'product price must be provided'],
-      enum: {
-         retails_price: 0,
-         wholesale_price: 0
-      }
-   },
-   quantity: {type: Number, require: true},
-   category: {type: String, require: true},
-})
+  name: { type: String, require: true },
+  price: {
+    retail_price: { type: Number, required: true },
+    wholeSale_price: { type: Number, required: true },
+  },
+  quantity: { type: Number, require: true },
+  category: { type: String, require: true },
+});
 
 export default mongoose.model('Product', productSchema)
 
