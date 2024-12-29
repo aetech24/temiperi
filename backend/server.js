8187a50aa5a8919f30571abe846d1101534bdb23
techpepson <ddpeprah@st.ug.edu.gh>
2024-12-29 11:30:29 +0000

    fix(conflicts): fix merge conflicts

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true,
   })
 );
