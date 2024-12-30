cbbf360668c2ed2dbe18f7f34612e11fc394017f
ephraim <aetech.hub@aetechorg.com>
2024-12-30 06:08:07 +0000

    refactoring the vite config for npm run build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+// import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [visualizer()],
+// });
+
+
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Group React libraries into a separate chunk
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // (Optional) Increase the warning limit
+  },
+});
