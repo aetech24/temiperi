d10ef07603bd6726bc19009b78c421faca90b686
techpepson <ddpeprah@st.ug.edu.gh>
2024-12-31 08:06:18 +0000

    feat(cors: confifure cors for vercel hosting)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,16 @@ import Product from "../models/productModel.js";
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({}).sort({ createdAt: -1 });
-    res.status(201).json({ products });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+    return res.status(201).json({ products });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error });
@@ -37,7 +46,6 @@ export const addProduct = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-
 
 //get a product
 export const getProduct = async (req, res) => {};
--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,19 @@ app.use("/temiperi", orderRouter);
 app.use("/temiperi", products);
 app.use("/temiperi", invoiceRouter);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("software is working");
 });
