a38b9374285fd860ac5f8ed8dfcdaea615ec9e68
techpepson <ddpeprah@st.ug.edu.gh>
2025-01-13 14:58:36 +0000

    fix(fix the product id again):

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -5,8 +5,8 @@ const orderSchema = new mongoose.Schema({
   paymentType: {
     type: String,
     required: true,
-    enum: ["full", "partial"],
-    default: "full",
+    enum: ['full', 'partial'],
+    default: 'full'
   },
   paymentMethod: {
     type: String,
@@ -16,12 +16,12 @@ const orderSchema = new mongoose.Schema({
   cashAmount: {
     type: Number,
     required: false,
-    default: 0,
+    default: 0
   },
   momoAmount: {
     type: Number,
     required: false,
-    default: 0,
+    default: 0
   },
   items: [
     {
